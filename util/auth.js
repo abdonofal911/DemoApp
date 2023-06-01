4e15cbb54c89ce4b4186a22a4aa7f4e381b876ac
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-06-01 13:25:18 +0300

    Changes to be committed: 	modified:   screens/SignupScreen.js 	modified:   util/auth.js

--- util/auth.js
+++ util/auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyBQ_m_GXdE2oNUF04nsz5C8pc4GNOZYXtI";
 
-async function createUser(email, password) {
+export async function createUser(email, password) {
   const response = await axios.post(
     "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
     {
