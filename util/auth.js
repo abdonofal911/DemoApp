5cb28e300c70b58fe4e11f1b9e46e8842edab934
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-06-03 13:40:37 +0300

    Changes to be committed: modified:   util/auth.js  Changes not staged for commit: 	modified:   screens/LoginScreen.js 	modified:   screens/SignupScreen.js

--- util/auth.js
+++ util/auth.js
@@ -3,21 +3,22 @@ import axios from "axios";
 const API_KEY = "AIzaSyBQ_m_GXdE2oNUF04nsz5C8pc4GNOZYXtI";
 
 async function authenticate(mode, email, password) {
-    const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
-  
-    const response = await axios.post(url, {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    });
-  
-    console.log(response.data);
-  }
-  
-  export async function createUser(email, password) {
-    await authenticate('signUp', email, password);
-  }
-  
-  export async function login(email, password) {
-    await authenticate('signInWithPassword', email, password);
-  }
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  const token = response.data.idToken;
+  return token;
+}
+
+export function createUser(email, password) {
+  authenticate("signUp", email, password);
+}
+
+export function login(email, password) {
+  return authenticate("signInWithPassword", email, password);
+}
