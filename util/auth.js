ba9370936374746251f89594e909a86d612a0d33
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-06-03 08:51:07 +0300

    Changes to be committed: 	modified:   screens/LoginScreen.js 	modified:   util/auth.js

--- util/auth.js
+++ util/auth.js
@@ -2,13 +2,20 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyBQ_m_GXdE2oNUF04nsz5C8pc4GNOZYXtI";
 
+async function authenticate(mode, email, password) {
+  url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+  console.log(response.data);
+}
+
 export async function createUser(email, password) {
-  const response = await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+  await authenticate("signUp", email, password);
+}
+export async function login(email, password) {
+  await authenticate("signInWithPassword", email, password);
 }
